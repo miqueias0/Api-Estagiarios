c6c14458ab642b8bc3946659ad4efb4e63dc8ab6
Miqueias <miqueiasnadaluti5@gmail.com>
2023-01-09 08:56:05 -0300

    Api

--- src/main/java/br/com/nsinova/api/App.java
+++ src/main/java/br/com/nsinova/api/App.java
@@ -4,6 +4,8 @@ import br.com.nsinova.allus.ws.rest.AutorizacaoFiltro;
 import br.com.nsinova.comum.webservice.rest.FiltroCORS;
 import br.com.nsinova.comum.webservice.rest.MapeadorGenericoExcecao;
 import br.com.nsinova.comum.webservice.rest.MapeadorObjetoResolvedorContexto;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -17,7 +19,10 @@ public class App extends ResourceConfig {
         register(MapeadorGenericoExcecao.class);
         register(AutorizacaoFiltro.class);
         register(FiltroCORS.class);
-        register(new MapeadorObjetoResolvedorContexto());
+        Map<String, String> opcoes = new HashMap<>();
+        opcoes.put("mostrar_atributo_nulo", "N");
+        opcoes.put("formato_data", "yyyy-MM-dd");
+        register(new MapeadorObjetoResolvedorContexto(opcoes));
     }
 
 }
